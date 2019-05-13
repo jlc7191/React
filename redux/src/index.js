f1e65f99171dbac6b8fa9e46f93214d3a7a1f840
jlc7191 <44053726+jlc7191@users.noreply.github.com>
2019-05-14 00:49:09 +0800

    0514

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,3 +1,6 @@
+//流程如下 1.import redux 2.撰寫reducer(類似store裡的資料夾) 3.拿寫好的reducer去建立redux的store (先斬後奏的概念) 
+//執行類似react的步驟 4.撰寫render函式讓狀態改變就重新渲染頁面 5.把render叫出來當作一開始的頁面 
+// 6.用subscribe把render跟store綁在一起這樣才會抓到狀態改變 7.用dispatch把新資料發送進store,進而觸發step4重新渲染頁面
 // step-1
 // 從redux模組中匯入createStore函式
 // combineReducers 是 chrome的redux除錯要用的
@@ -46,6 +49,7 @@ const store = createStore(
 // step-4
 // 撰寫一個render(渲染)函式,在狀態有更動時作重新呈現
 // store.getState() get state now
+// 這邊是去store拿資料, 然後render出來
 function render() {
     const items = store
         .getState()
@@ -60,15 +64,16 @@ function render() {
 render()
 
 // step-6
-// 訂閱render函式到store中
+// 訂閱render函式到store中 (綁定的概念)
 store.subscribe(render)
 
 // step-7
-//觸發事件時要呼叫store.dispatch(action)方法
+//觸發事件時要呼叫store.dispatch(action)方法 類似發送的感覺
 document.querySelector('#itemadd').addEventListener('click', () => {
     const itemText = document.querySelector('#itemtext').value
 
     // dispatch action
+    // 這邊就是itemadd這個按鈕被按下去, 就發送新的資料上去store ,然後讓第四步驟的render可以抓到狀態更動進而重新渲染
     store.dispatch(addItem(itemText))
 
     //clear input
